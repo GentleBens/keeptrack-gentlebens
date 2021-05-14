dffc4177111b7f81a78ff7943a03c6a461e1b003
stacyburris <stacy1burris@gmail.com>
2021-05-14 13:36:38 -0700

    new Date is being passed through sockets to the frontend

--- App.js
+++ App.js
@@ -1,36 +1,68 @@
-import React, { useEffect } from 'react';
+import React, {Component} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import CounterScreen from './src/CounterUsingRedux/CounterScreen';
 import DisplayContacts from './src/DisplayContacts/contacts';
 import store from './src/CounterUsingRedux/index';
-import { StatusBar, Image } from 'react-native';
+import { StatusBar, Image, Text } from 'react-native';
 import { Provider } from 'react-redux';
-//import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 //////
-const socket = io('http://localhost:5000/');
-const onChange = (e) => {
-  socket.emit('typing', {username:username})
-}
-useEffect(() => {
-  socket.on('someoneTyping', (user) => {
-    setTyping(user.username + 'is typing...') 
-  })
-},[])
+// import io from 'socket.io-client';
+// const socket = io('http://localhost:5000/');
+// const onChange = (e) => {
+//   socket.emit('typing', {username:username})
+// }
+// useEffect(() => {
+//   socket.on('someoneTyping', (user) => {
+//     setTyping(user.username + 'is typing...') 
+//   })
+// },[])
 
 //////
 
+import { subscribeToTimer } from './src/api';
 const Stack = createStackNavigator();
 
-export default function App(){
-    return (
+class App extends Component {
+constructor(props) {
+  super(props);
+
+  subscribeToTimer((err, timestamp) => {
+    this.setState({
+      timestamp
+    });
+  });
+}
+
+state = {
+  timestamp: 'no timestamp yet'
+};
+
+
+
+// const Stack = createStackNavigator();
+// export default function App(){
+// const [timestamp, setTimestamp] = useState('no timestamp yet');
+
+// // setTimestamp(timestamp);
+
+// const handleTimestamp = (props) => {
+// setTimestamp(subscribeToTimer)
+// }
+
+render(){
+   return (
       <Provider store={store}>
+            
+
       <StatusBar translucent backgroundColor='transparent' barStyle='light-content'
       />   
+         
            {/* <Image
     style={{width: 50, height: 30}}
     source={require('./src/assets/image/download.png')}/>     */}
-    {/* <SafeAreaProvider> */}
+    <SafeAreaProvider>
       <NavigationContainer>
           <Stack.Navigator>
             <Stack.Screen
@@ -38,19 +70,34 @@ export default function App(){
             options={{title: 'Gentle Ben\'s People Counter'}}
             component={CounterScreen}
             />
+         
             <Stack.Screen 
             name='Contacts' 
             component={DisplayContacts} 
             options={({ route }) => ({title: 'Contact List'})}
-        
-          
             />  
+           
+                 
           </Stack.Navigator>
+
+            
+            
         </NavigationContainer>
-        {/* </SafeAreaProvider> */}
-      </Provider>
+      
+     
+
+        </SafeAreaProvider>
+         <Text>'This is the value of the timer timestamp:' {this.state.timestamp} </Text>
+  
+</Provider>
+
     )
-}
+  }
+  // const styles = StyleSheet.create({
+    
+  //   )}
+}   
+export default App;
 
 
 // Provider = wrapper and responsible for making redux store accessible to the app
