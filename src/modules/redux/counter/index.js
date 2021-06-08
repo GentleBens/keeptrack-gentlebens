0995eda330366383b70e06be9fbac2e6ce0add7e
stacyburris <stacy1burris@gmail.com>
2021-06-08 13:48:31 -0700

    deleted zombie code

--- src/modules/redux/counter/index.js
+++ src/modules/redux/counter/index.js
@@ -1,9 +1,9 @@
 // IMPORT EXPORT STATEMENTS FOR ACTIONS & REDUCERS
-//, showModal, hideModal // , showModal, hideModal
-import { increment, decrement, reset, close, message } from './actions';
-//, createClick, createRemove
+
+import { increment, decrement, reset, close} from './actions';
+
 import reducer from './reducers';
 
-export { increment, decrement, reset, close, message};
-//, createClick, createRemove
+export { increment, decrement, reset, close};
+
 export default reducer;
