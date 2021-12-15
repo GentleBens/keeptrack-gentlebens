d299404d3265c8e2b413726e37a28faf13f67dd7
JP <jpjones22@gmail.com>
2021-12-14 19:03:06 -0800

    feat: 🎸 Display chart with sample data

--- src/ChartTotals/chartTotals.js
+++ src/ChartTotals/chartTotals.js
@@ -2,48 +2,83 @@ import React from 'react';
 import { Tab } from 'react-native-elements';
 import { Text, Image, View, Pressable, StyleSheet } from 'react-native';
 import chart from '../assets/image/chart.png';
+import { SimpleBarChart } from '@carbon/charts-react';
+import '@carbon/charts/styles.css'
+import store from '../CounterUsingRedux/index';
+
+
 
 export default function ChartTotals() {
+  let sampleData = {
+    data: [
+      {
+        "group": "12/12/2021",
+        "value": 10
+      },
+      {
+        "group": "12/13/2021",
+        "value": 12
+      },
+      {
+        "group": "12/14/2021",
+        "value": 20
+      }
+    ],
+    options: {
+      "title": "Attendance",
+      "axes": {
+        "left": {
+          "mapsTo": "value"
 
-    return (
-<>
-<Pressable
-      style={styles.container}
-    //onPress={saveCounterData}
+        },
+        "bottom": {
+          "mapsTo": "group",
+          "scaleType": "labels"
+        }
+      },
+      "height": "400px",
+      "width": "200px"
+    }
+  }
+  //store.dispatch({ type: 'server/getDataRange', clientCount: store.getState().counter.counter });
+
+
+
+  return (
+    <>
+      <Pressable
+        style={styles.container}
+      // onPress={getData}
       >
         <Text style={styles.button}>Get Totals</Text>
       </Pressable>
-    <Tab >
+      <Tab >
         <Tab.Item value={1} title='Day' />
         <Tab.Item value={2} title='Week' />
         <Tab.Item value={3} title='Month' />
 
-    </Tab>  
-
-   <View style={{paddingTop: 100, paddingLeft: 13, justifyContent:'center'}}>  
-        <Image
-        source={chart }
-        style={{ width: 350, height: 250 }}
-        />
- 
-     </View>   
-</>
-    )
+      </Tab>
+      <SimpleBarChart
+        data={sampleData.data}
+        options={sampleData.options}
+      />
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      alignItems: 'center',
-    },
-    button: {
-      borderRadius: 5,
-      borderWidth: 2,
-      borderColor: '#e7e7e7',
-      padding: 10,
-      elevation: 2,
-      margin: 4,
-      backgroundColor: "transparent",
-      color: "white"
-    },
-  
-  });
+  container: {
+    alignItems: 'center',
+  },
+  button: {
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#e7e7e7',
+    padding: 10,
+    elevation: 2,
+    margin: 4,
+    backgroundColor: "transparent",
+    color: "white"
+  },
+
+});
